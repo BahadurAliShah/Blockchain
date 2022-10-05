01aa2ad8e8e7d95b48ed24bd51972974f77da901
BahadurAliShah <sayyedbahadur@gmail.com>
2022-10-05 22:41:27 +0500

    add, list, change and verify functions added

--- Blockchain/Blockchain.go
+++ Blockchain/Blockchain.go
@@ -1,145 +1,148 @@
 package main
 
 import (
+	"bufio"
 	"crypto/sha256"
 	"fmt"
+	"os"
 	"strconv"
+	"strings"
 )
 
-type User struct {
-	name string
-	ID   int
+func CalculateHash(stringToHash string) string {
+	return fmt.Sprintf("%x", sha256.Sum256([]byte(stringToHash)))
 }
 
-func (u *User) AddUser(name string, ID int) *User {
-	u.name = name
-	u.ID = ID
-	return u
+type data struct {
+	previousHash string
+	transaction  string
+	nonce        int
 }
 
-func printUser(u User) {
-	fmt.Println("Name:", u.name, "\tID:", u.ID)
-}
-
-type Transaction struct {
-	From   User
-	To     User
-	Amount int
-}
-
-func (t *Transaction) AddTransaction(from User, to User, amount int) *Transaction {
-	t.From = from
-	t.To = to
-	t.Amount = amount
-	return t
-}
-
-func printTransaction(t Transaction) {
-	fmt.Println("From:", t.From.name, "\tTo:", t.To.name, "\tAmount:", t.Amount)
-}
-
-type Node struct {
-	Index        int
-	Transactions []Transaction
-	Hash         string
-	PrevHash     string
-	Nonce        string
-}
-
-func (n *Node) AddNode(index int, transactions []Transaction, prevHash string, nonce string) *Node {
-	n.Index = index
-	n.Transactions = transactions
-	n.PrevHash = prevHash
-	n.Nonce = nonce
-	n.Hash = n.calculateHash()
-	return n
-}
-
-func printNode(n Node) {
-	fmt.Println("Index:", n.Index, "\tPrevHash:", n.PrevHash, "\tHash:", n.Hash, "\tNonce:", n.Nonce)
-	for _, v := range n.Transactions {
-		printTransaction(v)
-	}
-}
-
-type merkleNode struct {
-	Left  *merkleNode
-	Right *merkleNode
-	Data  Node
-}
-
-func (m *merkleNode) AddMerkleNode(left *merkleNode, right *merkleNode, data Node) *merkleNode {
-	m.Left = left
-	m.Right = right
-	m.Data = data
-	return m
-}
-
-func (m *merkleNode) AddNode(node Node) *merkleNode {
-	if m.Left == nil && m.Right == nil {
-		return new(merkleNode).AddMerkleNode(nil, nil, node)
-	}
-	if m.Left != nil {
-		m.Left = m.Left.AddNode(node)
+func (b *data) AddBlock(previousHash string, transaction string, nonce int) *data {
+	b.transaction = transaction
+	b.nonce = nonce
+	if previousHash == "" {
+		b.previousHash = "0000000000000000000000000000000000000000000000000000000000000000"
 	} else {
-		m.Right = m.Right.AddNode(node)
+		b.previousHash = previousHash
 	}
-	return m
+	b.previousHash = previousHash
+	return b
 }
-func printMerkeNode(m *merkleNode) {
-	if m == nil {
+
+func (b *data) print() {
+	fmt.Println("Nonce: ", b.nonce)
+	fmt.Println("Transaction: ", b.transaction)
+	fmt.Println("Previous Hash: ", b.previousHash)
+}
+
+type block struct {
+	nodes []data
+}
+
+var BlockChain = new(block)
+
+func NewBlock(transaction string, nonce int, previousHash string) *block {
+	BlockChain.nodes = append(BlockChain.nodes, *new(data).AddBlock(previousHash, transaction, nonce))
+	return BlockChain
+}
+
+func ListBlocks() {
+	if len(BlockChain.nodes) == 0 {
+		fmt.Println("No Blocks")
 		return
 	}
-	printNode(m.Data)
-	printMerkeNode(m.Left)
-	printMerkeNode(m.Right)
+	for i, v := range BlockChain.nodes {
+		fmt.Println("Node: ", i)
+		v.print()
+		fmt.Println(strings.Repeat("-", 50))
+	}
 }
 
-type Blockchain struct {
-	root *merkleNode
-}
-
-func printBlockchain(b Blockchain) {
-	printMerkeNode(b.root)
-}
-
-// add node to blockchain
-func (b *Blockchain) AddNode(node Node) {
-	//add node to merkle tree
-	if b.root == nil {
-		b.root = new(merkleNode).AddMerkleNode(nil, nil, node)
+func ChangeBlock(ID int, transaction string, nonce int, previousHash string) {
+	if ID >= len(BlockChain.nodes) || ID < 0 {
+		fmt.Println("Invalid ID")
+		return
 	} else {
-		b.root = b.root.AddNode(node)
+		BlockChain.nodes[ID].AddBlock(previousHash, transaction, nonce)
 	}
 }
 
-func (n *Node) calculateHash() string {
-	//calculate hash of node
-	string := strconv.Itoa(n.Index) + n.PrevHash + n.Nonce
-	for _, v := range n.Transactions {
-		string += v.From.name + v.To.name + strconv.Itoa(v.Amount)
+func VerifyChain() bool {
+	for i, v := range BlockChain.nodes {
+		if i == 0 {
+			continue
+		}
+		if v.previousHash != CalculateHash(BlockChain.nodes[i-1].transaction+strconv.Itoa(BlockChain.nodes[i-1].nonce)) {
+			return false
+		}
 	}
-	hash := sha256.Sum256([]byte(string))
-	return fmt.Sprintf("%x", hash)
+	return true
+}
+
+func printMenu() int {
+	fmt.Println("1. Add Block")
+	fmt.Println("2. List Blocks")
+	fmt.Println("3. Change Block")
+	fmt.Println("4. Verify Chain")
+	fmt.Println("5. Exit")
+	var choice int
+	fmt.Scanln(&choice)
+	for choice < 1 || choice > 5 {
+		fmt.Println("Invalid Choice")
+		fmt.Scanln(&choice)
+	}
+	return choice
+}
+
+func getTopHash() string {
+	if len(BlockChain.nodes) == 0 {
+		return "0000000000000000000000000000000000000000000000000000000000000000"
+	}
+	return CalculateHash(BlockChain.nodes[len(BlockChain.nodes)-1].transaction + strconv.Itoa(BlockChain.nodes[len(BlockChain.nodes)-1].nonce))
 }
 
 func main() {
-	//create blockchain
-	b := Blockchain{nil}
-	//create users
-	u1 := new(User).AddUser("Bob", 1)
-	u2 := new(User).AddUser("Alice", 2)
-	u3 := new(User).AddUser("John", 3)
-	//create transactions
-	t1 := new(Transaction).AddTransaction(*u1, *u2, 10)
-	t2 := new(Transaction).AddTransaction(*u2, *u3, 20)
-	t3 := new(Transaction).AddTransaction(*u3, *u1, 30)
-	//create nodes
-	n1 := new(Node).AddNode(1, []Transaction{*t1, *t2}, "", "")
-	n2 := new(Node).AddNode(2, []Transaction{*t3}, n1.Hash, "")
-	//add nodes to blockchain
-	b.AddNode(*n1)
-	b.AddNode(*n2)
-
-	//print blockchain
+	for {
+		switch printMenu() {
+		case 1:
+			var transaction string
+			var nonce int
+			fmt.Println("Enter Transaction: ")
+			scanner := bufio.NewScanner(os.Stdin)
+			scanner.Scan()
+			transaction = scanner.Text()
+			fmt.Println("Enter Nonce: ")
+			fmt.Scanln(&nonce)
+			NewBlock(transaction, nonce, getTopHash())
+		case 2:
+			ListBlocks()
+		case 3:
+			var ID int
+			var transaction string
+			var nonce int
+			if len(BlockChain.nodes) == 0 {
+				fmt.Println("No Blocks")
+				break
+			}
+			fmt.Println("Enter ID: ")
+			fmt.Scanln(&ID)
+			fmt.Println("Enter Transaction: ")
+			scanner := bufio.NewScanner(os.Stdin)
+			scanner.Scan()
+			transaction = scanner.Text()
+			fmt.Println("Enter Nonce: ")
+			fmt.Scanln(&nonce)
+			ChangeBlock(ID, transaction, nonce, "")
+		case 4:
+			if VerifyChain() {
+				fmt.Println("Chain is Valid")
+			} else {
+				fmt.Println("Chain is Invalid")
+			}
+		case 5:
+			return
+		}
+	}
 }
